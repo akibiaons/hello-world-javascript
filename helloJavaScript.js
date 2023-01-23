6417ecc910462341b40854c45923aaecf4eec92f
Arthur Fernandez <arthur@snooget.com>
2023-01-22 20:50:43 -0800

    added new console.log comments

--- helloJavaScript.js
+++ helloJavaScript.js
@@ -29,3 +29,9 @@ console.log(Math.ceil(43.8));
 /*
 The statement above returns the smallest integer greater than or equal to a decimal number...
 */
+new Int32Array([1.1, 1.9, -1.1, -1.9]); // Int32Array(4) [ 1, 1, -1, -1 ]
+new Int8Array([257, -257]); // Int8Array(1) [ 1, -1 ]
+// 257 = 0001 0000 0001 = 0000 0001 (mod 2^8) = 1
+// -257 = 1110 1111 1111 = 1111 1111 (mod 2^8) = -1 (as signed integer)
+new Uint8Array([257, -257]); // Uint8Array(1) [ 1, 255 ]
+// -257 = 1110 1111 1111 = 1111 1111 (mod 2^8) = 255 (as unsigned integer)
