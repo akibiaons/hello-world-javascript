cc17121b5dbdff52da7ad8b963f480faea5dd5d8
Arthur Fernandez <arthur@snooget.com>
2023-01-23 12:08:00 -0800

    added new comments to explain the codechallenge

--- codechallenge1.js
+++ codechallenge1.js
@@ -1,2 +1,10 @@
 // The first challenge is to log "Hello World my name is ${name} into the console with console.log();"
 console.log('Hello World my name is Arthur');
+// END OF CODE CHALLENGE A FOR CODECHALLENGE 1:
+//===============================================================================
+
+/*
+Part B will require that we use comments to explain a script that calculates the area of a circle with
+radius of 5.
+*/
+
