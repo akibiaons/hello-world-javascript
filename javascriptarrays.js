2c9cf0f914b563961bea30cdd436a813fde928ce
Arthur Fernandez <arthur@snooget.com>
2023-01-25 13:31:27 -0800

    added arrays notes, and made my first array

---
+++ javascriptarrays.js
@@ -0,0 +1,14 @@
+// WILL BE CREATING MY FIRST ARRAY
+
+/* 
+One way to create an array is by using an array literal 
+an array literal creates an array that is surrounded by
+these brackets --> [].
+
+- Arrays just hold data types. 
+- Arrays can hold the same or different data types.
+*/
+['element example', 10, true] // element example, 10, and true are all individual array elements.
+
+// An array can also be shared to a variable as seen below:
+let newYearsResolutions = ['Keep a journal', 'Take a falconry class', 'Learn to juggle'];
