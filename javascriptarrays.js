b5f56d3581d0080175cc87ff9e6c34b6dbc26006
Arthur Fernandez <arthur@snooget.com>
2023-01-26 11:22:06 -0800

    just added some space to the console

--- javascriptarrays.js
+++ javascriptarrays.js
@@ -1,22 +1,17 @@
 // WILL BE CREATING MY FIRST ARRAY
-
 /* 
-
 One way to create an array is by using an array literal 
 an array literal creates an array that is surrounded by
 these brackets --> [].
 - Arrays just hold data types. 
 - Arrays can hold the same or different data types.
-
 */
-
 ['element example', 10, true] // element example, 10, and true are all individual array elements.
 
 // An array can also be shared to a variable as seen below:
 let newYearsResolutions = ['Keep a journal', 'Take a falconry class', 'Learn to juggle'];
 console.log(newYearsResolutions);
 // END OF LESSON ONE 
-
 //=========================================================================================================
 
 // START OF LESSON TWO: ACCESSING ELEMENTS
@@ -34,5 +29,8 @@ let cities = ['New York' /*New York is index 0*/, 'San Francisco'/*SF is index 1
 cities[0] // 0 is an index number which contains the string 'New York'...
 // You can also access specific characters in a string using bracket notation and the index.
 const hi = 'Hello World';
-console.log(hi[4]);
+console.log(hi[6]);
 // Output: W, If i add spacing and , it will pick them up as characters too...
+
+// END OF LESSON TWO: ACCESSING ELEMENTS
+//==========================================================================================================================================
