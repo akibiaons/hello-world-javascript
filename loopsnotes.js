e25c8282c14f6604a72fb0634aa550669e163edd
Arthur Fernandez <arthur@snooget.com>
2023-02-03 20:49:31 -0800

    just added some new notes with an array and console.log(); explaining the usefullness of loops

---
+++ loopsnotes.js
@@ -0,0 +1,23 @@
+/*
+- Loops allow for us to create a set of instructions to be run until a certain condition is met.
+- Loops allow us developers to create scalable, manageable, and efficent JavaScript programs. 
+*/
+
+//LESSON 1: Repeating Tasks Manually
+/*
+- Before making loops we must understand that writing the same code over and over agin can vbe tiring as seen below:
+*/
+
+const cities = ['San Diego', 'Bakersfeild', 'San Fransisco', 'Los Angeles'];
+console.log(cities[0]);
+console.log(cities[1]);
+console.log(cities[2]);
+console.log(cities[3]);
+// Above I console.log(cities[index]); for each and every indexed element. This is a tiresome and non scalable way of programming with JavaScript.
+// END OF LESSON 1: Repeating Tasks Manually.
+//===================================================================================================================================================
+
+// LESSON 2: The For Loop
+/*
+- 
+*/
