9eadcf15a60e680611a4b4888854aea99bd94066
Arthur Fernandez <arthur@snooget.com>
2023-02-15 11:47:52 -0800

    just added some arrays

--- whale-talk-project.js
+++ whale-talk-project.js
@@ -3,7 +3,6 @@ This project will be an english to whale talk translator...
 
 - We will use loops and arrays to create this language translator.
 */
-
 // First we will make a varible named input equal to any string we like.
 let input = 'I like latinas!';
 // Step two will have us create an array named vowles to hold the 'a, e, i, o, u' vowles spoken by whales..
