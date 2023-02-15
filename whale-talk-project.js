c4b1803dfd3b16a868959e19fd5ccd81b4ba3385
Arthur Fernandez <arthur@snooget.com>
2023-02-14 19:22:09 -0800

    just added a varible

---
+++ whale-talk-project.js
@@ -0,0 +1,10 @@
+/* 
+This project will be an english to whale talk translator...
+
+- We will use loops and arrays to create this language translator.
+*/
+
+// First we will make a varible named input equal to any string we like.
+
+let input = 'I like latinas!';
+
