a44a6442bd8e1118ba1c701d20e17445589ee5f2
Arthur Fernandez <arthur@snooget.com>
2023-02-24 21:47:58 -0800

    just added a new object literal

---
+++ objectsnotes.js
@@ -0,0 +1,16 @@
+// PART 1: Introduction to Objects in JavaScript
+/*
+- Objects are the most basic structure that permeates nearly every aspect of JavaScript.
+- At their core, objects are containers storing related data and functionality.
+*/
+// Start of Lesson 1: Creating Object Literals
+/*
+- Objects can be assigned to any variable type and we use {} brackets to assign an object literal.
+- We then fill an object with unordered data that is organized by key-value pairs.
+Below is an example of an object literal being created:
+*/
+let spaceShip = {
+    'Rocket': 'Turbo Fuel',
+    color: 'silver'
+};
+console.log(spaceShip);
