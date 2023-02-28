1abe55a6c7dc3fa7ae651a088c49661ea9f7ade0
Arthur Fernandez <arthur@snooget.com>
2023-02-28 15:08:32 -0800

    Just added an example of creating an object method with a function in the old : () and new () method after ES6. I additionally added an example of invoking the method to the console

--- objectsnotes.js
+++ objectsnotes.js
@@ -109,9 +109,42 @@ Objects assigned to properties are mutable and can be changed after they are wri
     Also note that you cannot change the property of an object asssigned with the const variable. But it is mutable.
     Below is the perfect example:
     */
-   const mercedesShip = {type: 'Luxury Shuttle'};
+   const mercedesShip = {
+    'Fuel Type': 'Rocket Fuel',
+    type: 'Luxury Shuttle'
+    };
     //mercedesShip = 'Alien'; // This will create an error.
      mercedesShip.type = 'Luxury AV'; // This will change the type property to 'Luxury AV'
      mercedesShip.speed = '50 Mach'; // This will change the speed property to '50 Mach'
      //console.log(mercedesShip.type); // Will print 'Luxury AV'
      //console.log(mercedesShip.speed); // Will print '50 Mach'
+// A property can also be deleted with the delete operator.
+    delete mercedesShip.type; // Will delete the type property.
+    delete mercedesShip['Fuel Type']; // Will delete the 'Fuel Type' property.
+    //console.log(mercedesShip.type); // Will return undefined.
+// End of Lesson 4: Property Assignment
+//=================================================================================================================================
+// Start of Lesson 5: Methods
+/* 
+When the data stored on an object is a function we call it a method. A property is what an 
+object has, while a method is what an object does.
+
+We have been using object methods throughout the whole course.
+  - An example of this is with the .log() method when adding info to the console. console is a global JavaScript object.
+  - .floor() is another example of a method is with the Math object.
+Below is an example of creating ordinary, colon seperated key-value pairs.
+*/
+const alienShip = {
+    invade: function () {
+        console.log('You have been invaded!');
+    }
+};
+// With the new method syntax we can ommit the colon and function keyword.
+const alienShip2 = {
+    invade () {
+        console.log('Listen to our demands and you will be spared');
+    }
+}
+alienShip.invade(); 
+alienShip2.invade();
+// To invoke the object methods we just need to append the object's name with the method name and a parenthesis.
