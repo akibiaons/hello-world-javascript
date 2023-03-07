854b713ea19f713aca3db1edd31a1b759602cfb4
Arthur Fernandez <arthur@snooget.com>
2023-03-06 21:10:46 -0800

    added in some extra notes to recap what we learned with objects

--- objectsnotes.js
+++ objectsnotes.js
@@ -298,3 +298,19 @@ let starShip = {
 for (let crewMember in starShip.crew) {
     console.log(`${crewMember}: ${starShip.crew[crewMember].name}`);
 }
+// End of Lesson 8: Looping Through Objects
+//===================================================================================================================
+// Congrats on completing the objects section!
+/* 
+Here is a quick review of what was learned in this lesson.
+    - Objects store collections of key-value pairs.
+    - Each key-value pair is a property when a property is a function it is known as a method.
+    - An object literal is composed of comma-sepaerated key-value pairs surrounded by curly braces.
+    - You can access, add, or edit a property within an object by using dot notation or bracket notation.
+    - We can add methods to our object literals using key-value syntax with anonomous function expressions as 
+    values or by using the new ES6 method syntax.
+    - We can navigate complex, nested objects by chaining operators.
+    - Objects are mutable-we can change their properties even when their declared with const.
+    - Objects are passed by reference, when we make changes to an object passed into a function, those changes are permanent.
+    - We can iterate through objects using the for...in syntax.
+*/ 
