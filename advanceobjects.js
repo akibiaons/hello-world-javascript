8b9d9dd8b6e23be4865e38a0fc97c714af06afde
Arthur Fernandez <arthur@snooget.com>
2023-03-07 17:25:23 -0800

    just added some comments about advance objects and the goal of the file

---
+++ advanceobjects.js
@@ -0,0 +1,6 @@
+// We will now be moving on to the next learning objective, which is advance objects.
+/*
+Remember that objects in JavaScript are containers that store data and functionality.
+In this lesson we will build upon the fundamentals of building objects and explore some 
+more advance concepts.
+*/
