6e23c09a0f4b9b6c27cb0bd30e40b45fab5e1329
Arthur Fernandez <arthur@snooget.com>
2023-04-15 15:44:35 -0700

    added example of .every() iterator method

--- react-notes/iterators-test.js
+++ react-notes/iterators-test.js
@@ -3,3 +3,11 @@ const randomNums = [1, 123, 25, 90, 3543, 42];
 const foundElement = randomNums.findIndex(num => num > 200);
 // This code will output...
 console.log(foundElement);
+
+// Below I will be testing the .every() method before implementing it in a grammar checker program...
+const isBelowThreshold = (currentValue) => currentValue < 40;
+
+const array1 = [1, 30, 39, 29, 10, 13];
+
+console.log(array1.every(isBelowThreshold));
+// Expected output: true
